26150c9e6233519219ff2c3f3982b12a2872a7ad
Robert Butler <robwbutler@gmail.com>
2019-05-17 13:24:39 -0400

    built survey framework, posts to json

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friends = [
+    {
+      name: "Rusty Shackleford",
+      photo: "https://via.placeholder.com/350x350",
+      scores: [
+        "1", 
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9"]
+    },
+  ];
+
+  module.exports = friends
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,23 @@
 var express = require("express")
 var path = require("path");
 var apirouter = express.Router()
+var friends = require("../data/friends")
 
 
 apirouter.get("/api/friends", function(req, res) {
-    res.send(("json here eventually"));
-});
+    return res.json(friends);
+  });
 
+apirouter.post("/api/friends", function(req, res) {
+
+    var newfriend = req.body;
+  
+    console.log(newfriend);
+  
+    friends.push(newfriend);
+  
+    res.json(newfriend);
+
+  });
 
 module.exports = apirouter
