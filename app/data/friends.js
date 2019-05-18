77ed74b5dbce5c0d4ed16c7fcb69ed3fca51f0f4
Robert Butler <robwbutler@gmail.com>
2019-05-18 14:55:46 -0400

    friend match checks score difference and returns closest match

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,15 +3,61 @@ var friends = [
       name: "Rusty Shackleford",
       photo: "https://via.placeholder.com/350x350",
       scores: [
-        "1", 
-        "2",
-        "3",
-        "4",
+        "5", 
         "5",
-        "6",
-        "7",
-        "8",
-        "9"]
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5"]
+    },
+    {
+      name: "Slippy",
+      photo: "https://via.placeholder.com/350x350",
+      scores: [
+        "4", 
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4"]
+    },
+    {
+      name: "Testy McTestFace",
+      photo: "https://via.placeholder.com/350x350",
+      scores: [
+        "3", 
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3"]
+    },
+    {
+      name: "Deuces",
+      photo: "https://via.placeholder.com/350x350",
+      scores: [
+        "2", 
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2"]
     },
   ];
 
