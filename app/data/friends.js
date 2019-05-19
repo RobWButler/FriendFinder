b1371e448a7e3d874520f9dcff0c6c3821f464fb
Robert Butler <robwbutler@gmail.com>
2019-05-18 21:55:12 -0400

    added questions and tweaked example scores

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,66 +3,6 @@ var friends = [
       name: "Johnny Fiveaces",
       photo: "https://i.imgur.com/HKnBR01.jpg",
       scores: [
-        "5", 
-        "5",
-        "5",
-        "5",
-        "5",
-        "5",
-        "5",
-        "5",
-        "5",
-        "5"]
-    },
-    {
-      name: "Rusty Shackleford",
-      photo: "https://i.imgur.com/VDtEn9w.jpg",
-      scores: [
-        "4", 
-        "4",
-        "4",
-        "4",
-        "4",
-        "4",
-        "4",
-        "4",
-        "4",
-        "4"]
-    },
-    {
-      name: "Slippy",
-      photo: "https://i.imgur.com/2zCW2h3.jpg",
-      scores: [
-        "3", 
-        "3",
-        "3",
-        "3",
-        "3",
-        "3",
-        "3",
-        "3",
-        "3",
-        "3"]
-    },
-    {
-      name: "JC Denton",
-      photo: "https://i.imgur.com/smHi7Aq.png",
-      scores: [
-        "2", 
-        "2",
-        "2",
-        "2",
-        "2",
-        "2",
-        "2",
-        "2",
-        "2",
-        "2"]
-    },
-    {
-      name: "Hal Emmerich",
-      photo: "https://i.imgur.com/PSHNXHE.jpg",
-      scores: [
         "1", 
         "1",
         "1",
@@ -72,6 +12,66 @@ var friends = [
         "1",
         "1",
         "1",
+        "5"]
+    },
+    {
+      name: "Rusty Shackleford",
+      photo: "https://i.imgur.com/VDtEn9w.jpg",
+      scores: [
+        "5", 
+        "5",
+        "4",
+        "5",
+        "5",
+        "2",
+        "3",
+        "1",
+        "1",
+        "5"]
+    },
+    {
+      name: "Slippy",
+      photo: "https://i.imgur.com/2zCW2h3.jpg",
+      scores: [
+        "1", 
+        "5",
+        "3",
+        "1",
+        "2",
+        "3",
+        "5",
+        "4",
+        "2",
+        "3"]
+    },
+    {
+      name: "JC Denton",
+      photo: "https://i.imgur.com/smHi7Aq.png",
+      scores: [
+        "3", 
+        "2",
+        "2",
+        "5",
+        "3",
+        "4",
+        "5",
+        "4",
+        "5",
+        "1"]
+    },
+    {
+      name: "Hal Emmerich",
+      photo: "https://i.imgur.com/PSHNXHE.jpg",
+      scores: [
+        "1", 
+        "4",
+        "5",
+        "1",
+        "3",
+        "5",
+        "5",
+        "1",
+        "2",
         "1"]
     },
   ];
